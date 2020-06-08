36305b58c74870f9060618d40e1227f13f539fa1
eliozola <ozola.eli@gmail.com>
2020-06-08 12:57:21 +0300

    commit

---
+++ public/syntax.php
@@ -0,0 +1,9 @@
+<?php 
+echo "syntax!" <hr>
+echo ("text to display");
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+$result = $x + $y;
+echo "your greeting is $txt, and $x + $y" = $result.";  
+var_dump($text); 
